6ee763a3985616aaf4e82f1b951f0467ea426e31
brncd <brunocarnales@gmail.com>
2023-02-25 09:44:16 -0800

    task 5

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """Base Model"""
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -19,6 +20,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            storage.new(self)
 
     def __str__(self):
         """String representation"""
@@ -35,3 +37,7 @@ class BaseModel:
         my_dict["created_at"] = self.created_at.isoformat()
         my_dict["updated_at"] = self.updated_at.isoformat()
         return my_dict
+
+    def storage(self):
+        """Storage"""
+        storage.save()
