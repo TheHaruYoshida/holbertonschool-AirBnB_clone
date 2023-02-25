571f0c172c3dcc696ce3682877be5e4a981b4feb
brncd <brunocarnales@gmail.com>
2023-02-25 07:13:34 -0800

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -13,9 +13,12 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """String representation"""
